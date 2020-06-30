4c0715a4507ecd2371dcf4839ff7c5c35ed44594
Alexis Kim <ajkim0630@gmail.com>
2020-06-29 23:55:59 -0700

    brute force circular suffix array works

--- algorithms2/burrows/CircularSuffixArray.java
+++ algorithms2/burrows/CircularSuffixArray.java
@@ -1,25 +1,31 @@
 package algorithms2.burrows;
 
+import java.util.Arrays;
+
 public class CircularSuffixArray {
     private String string;
     private String[] original;
+    private String[] sorted;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
         if (s == null) throw new IllegalArgumentException();
         string = s;
         original = new String[string.length()];
+        sorted = new String[string.length()];
 
         for (int i = 0; i < string.length(); i++) {
             StringBuilder st = new StringBuilder();
-            for (int j = string.length() - i; j < string.length(); j++) {
+            for (int j = i; j < string.length(); j++) {
                 st.append(string.charAt(j));
             }
-            for (int j = 0; j < string.length() - i; j++) {
+            for (int j = 0; j < i; j++) {
                 st.append(string.charAt(j));
             }
             original[i] = st.toString();
+            sorted[i] = st.toString();
         }
+        Arrays.sort(sorted);
 
     }
 
@@ -32,13 +38,19 @@ public class CircularSuffixArray {
     public int index(int i) {
         if (i < 0 || i > string.length()-1) throw new IllegalArgumentException();
 
+        String wanted_string = sorted[i];
 
-        return 0;
+        for (int j = 0; j < length(); j++) {
+            if (original[j].equals(wanted_string)) return j;
+        }
+        throw new IllegalArgumentException();
+
     }
 
     // unit testing (required)
     public static void main(String[] args) {
         CircularSuffixArray a = new CircularSuffixArray("ABRACADABRA!");
+        System.out.println(a.index(11));
 
 
     }
