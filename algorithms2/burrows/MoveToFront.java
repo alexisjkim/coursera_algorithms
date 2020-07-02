23a2288139913c85f8fec877e856cbb79b270b05
Alexis Kim <ajkim0630@gmail.com>
2020-07-01 18:13:42 -0700

    fixed decode in movetofront

--- algorithms2/burrows/MoveToFront.java
+++ algorithms2/burrows/MoveToFront.java
@@ -15,26 +15,12 @@ public class MoveToFront {
             char c = BinaryStdIn.readChar();
             char buffer = alphabet[0];
 
-       //     if (alphabet[0] == c) BinaryStdOut.write((char) 0);
 
             int x = 0;
 
             while (alphabet[x] != c) x++;
             char x_value = alphabet[x];
 
-            // for (int i = 0; i < 256; i++) {
-            //     // char temp_buffer = alphabet[i];
-            //     // alphabet[i] = buffer;
-            //     // buffer = temp_buffer;
-            //
-            //     if (alphabet[i] == c) {
-            //         BinaryStdOut.write((char) i);
-            //         x = i;
-            //         x_value = alphabet[i];
-            //         // alphabet[0] = c;
-            //         // alphabet[i+1] = buffer;
-            //         break;
-            //     }
 
             BinaryStdOut.write((char) x);
             System.arraycopy(alphabet, 0, alphabet, 1, x);
@@ -53,17 +39,17 @@ public class MoveToFront {
         for (char i = 0; i < 256; i++) { // initializing alphabet values
             alphabet[i] = i;
         }
+
         while (!BinaryStdIn.isEmpty()) {
             int c = BinaryStdIn.readChar();
-            char buffer = alphabet[0];
 
+            char c_value = alphabet[c];
             BinaryStdOut.write(alphabet[c]);
 
-            for (int i = 1; i < c; i++) {
-                char temp_buffer = alphabet[i];
-                alphabet[i] = buffer;
-                buffer = temp_buffer;
-            }
+            System.arraycopy(alphabet, 0, alphabet, 1, c);
+            alphabet[0] = c_value;
+
+
         }
 
         BinaryStdIn.close();
