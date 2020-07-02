1fa1168d86fe05bebc32303c3c3b58f089e76d53
Alexis Kim <ajkim0630@gmail.com>
2020-07-02 01:00:41 -0700

    burrows wheeler decoding works on first half but fails in middle

--- algorithms2/burrows/BurrowsWheeler.java
+++ algorithms2/burrows/BurrowsWheeler.java
@@ -43,7 +43,39 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
+        int first = BinaryStdIn.readInt();
+        String string = BinaryStdIn.readString();
+        char[] sorted_suffix = new char[string.length()];
+        for (int i = 0; i < string.length(); i++) sorted_suffix[i] = string.charAt(i);
+        Arrays.sort(sorted_suffix);
 
+        int[] next = new int[string.length()];
+        next[0] = first;
+
+        StringBuilder st = new StringBuilder();
+
+        int count = 0;
+        for (int i = 1; i < string.length(); i++) { // making next[] list
+            if (sorted_suffix[i] == sorted_suffix[i-1]) count++;
+            else count = 0;
+
+            int current_count = 0;
+            for (int j = 0; j < string.length(); j++) {
+                if (string.charAt(j) == sorted_suffix[i]) current_count++;
+                if (current_count > count) {
+                    next[i] = j;
+                    break;
+                }
+            }
+        }
+        int next_index = first;
+        for (int i = 0; i < string.length(); i++) {
+            st.append(sorted_suffix[next_index]);
+            next_index = next[next_index];
+        }
+        BinaryStdOut.write(st.toString());
+        BinaryStdIn.close();
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
