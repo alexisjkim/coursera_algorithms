3ea1066eb559fd3a4810285d8ba761e801586737
Alexis Kim <ajkim0630@gmail.com>
2020-07-01 18:40:22 -0700

    transform in burrows-wheeler works:

--- algorithms2/burrows/BurrowsWheeler.java
+++ algorithms2/burrows/BurrowsWheeler.java
@@ -1,9 +1,42 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.Arrays;
 
 public class BurrowsWheeler {
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform() {
+        String string = BinaryStdIn.readString();
+
+        String[] sorted = new String[string.length()];
+
+        for (int i = 0; i < string.length(); i++) {
+            StringBuilder st = new StringBuilder();
+            for (int j = i; j < string.length(); j++) {
+                st.append(string.charAt(j));
+            }
+            for (int j = 0; j < i; j++) {
+                st.append(string.charAt(j));
+            }
+            sorted[i] = st.toString();
+        }
+        Arrays.sort(sorted);
+
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < sorted.length; i++) {
+            if (sorted[i].equals(string)) BinaryStdOut.write(i);
+            st.append(sorted[i].charAt(string.length() - 1));
+
+        }
+        BinaryStdOut.write(st.toString());
+        BinaryStdIn.close();
+        BinaryStdOut.close();
+
+
+
+
 
     }
 
@@ -16,6 +49,8 @@ public class BurrowsWheeler {
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
+        if (args[0].equals("-")) BurrowsWheeler.transform();
+        if (args[0].equals("+")) BurrowsWheeler.inverseTransform();
 
     }
 
